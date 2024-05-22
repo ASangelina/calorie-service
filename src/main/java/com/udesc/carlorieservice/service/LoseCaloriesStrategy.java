ec63651c6bd540d5bf6d078f3086172fec3f5938
Angelina <machadoangelina321@gmail.com>
2024-05-22 18:48:13 -0300

    adjuted Lose calories

--- src/main/java/com/udesc/carlorieservice/service/LoseCaloriesStrategy.java
+++ src/main/java/com/udesc/carlorieservice/service/LoseCaloriesStrategy.java
@@ -3,8 +3,8 @@ package com.udesc.carlorieservice.service;
 public class LoseCaloriesStrategy implements CalorieStrategy{
     @Override
     public int[] calculate(int currentCalories) {
-        int minCalories = currentCalories - 1000;
-        int maxCalories = currentCalories - 500;
+        int minCalories = currentCalories - 500;
+        int maxCalories = currentCalories - 1000;
         return new int[]{minCalories, maxCalories};
     }
 }
